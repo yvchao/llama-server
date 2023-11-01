f17d684f196c7f22952fb8f3c0852d651b4f0998
Yuchao Qin <yvchao@outlook.com>
2023-11-01 22:03:15 +0000

    project: update code structure
    Signed-off-by: Yuchao <yvchao@outlook.com>

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+import re
+
+from setuptools import Extension, setup
+
+if __name__ == "__main__":
+    try:
+        PKG = "llama_server"
+
+        # Configure version.
+        def find_version() -> str:
+            def read(fname: str) -> str:
+                return open(os.path.realpath(os.path.join(os.path.dirname(__file__), fname)), encoding="utf8").read()
+
+            version_file = read(f"src/{PKG}/version.py").split("\n")[0]
+            version_re = r"__version__ = \"(?P<version>.+)\""
+            version_raw = re.match(version_re, version_file)
+
+            if version_raw is None:
+                raise RuntimeError(f"__version__ value not found, check src/{PKG}/version.py")
+
+            version = version_raw.group("version")
+            return version
+
+        setup(
+            version=find_version(),
+        )
+    except:  # noqa
+        print("\n\nAn error occurred while building the project")
+        raise
